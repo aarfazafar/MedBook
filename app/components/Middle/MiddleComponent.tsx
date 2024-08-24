271672edc84ae61c0f83a0fe8422861ee42dc644
T-Tanweer04 <tanweerjamal833@gmail.com>
2024-08-25 00:14:52 +0530

    Create post button

--- app/components/Middle/MiddleComponent.tsx
+++ app/components/Middle/MiddleComponent.tsx
@@ -5,6 +5,10 @@ import { SessionProvider } from 'next-auth/react'
 import prisma from '@/app/lib/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/dist/server/api-utils'
+import PostComponent from '../Posts/PostComponent'
+import PostCreationDialogue from '../Posts/PostCreationDialog'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -12,7 +16,9 @@ const MiddleComponent = (props: Props) => {
   return (
     <div>
       <SessionProvider>
-        <NewPost/>
+        <Button className='w-full'>
+          <Link href="/r/id/create" className="w-full">Create Post</Link>
+        </Button>
       </SessionProvider>
     </div>
   )
